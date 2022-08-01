05613d542502a53ca29b35fd070d5b277d789caa
Emin <ahbabovicemin2005@gmail.com>
2022-08-01 14:41:20 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.76173d4c2ac41e2bcab10343c0945f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75b15698abf2b1c09d29",
+    "url": "/Quizzical/static/css/main.96ba1966.chunk.css"
+  },
+  {
+    "revision": "75b15698abf2b1c09d29",
+    "url": "/Quizzical/static/js/main.75b15698.chunk.js"
+  },
+  {
+    "revision": "7799d9e812df4e12802c",
+    "url": "/Quizzical/static/js/1.7799d9e8.chunk.js"
+  },
+  {
+    "revision": "99bdfaf528fc7d308811",
+    "url": "/Quizzical/static/js/2.99bdfaf5.chunk.js"
+  },
+  {
+    "revision": "dee80c63878bde555633",
+    "url": "/Quizzical/static/js/runtime~main.dee80c63.js"
+  },
+  {
+    "revision": "857993d224beac8c1104ab3ba1ce33d8",
+    "url": "/Quizzical/index.html"
+  }
+];
